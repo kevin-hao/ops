4b4a879fcdeef89d139342f358594a155c002a74
kevin-hao <kevinhao@aliyun.com>
2021-03-08 11:29:12 +0800

    add element

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './plugins/element.js'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
