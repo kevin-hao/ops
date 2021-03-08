8c6c78777bf86dff6bdcfcd31666ec8b5c574db8
kevin-hao <kevinhao@aliyun.com>
2021-03-08 14:01:31 +0800

    use element

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './plugins/element.js'
+import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import router from './router'
 
-Vue.use(ElementUI)
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
 new Vue({
+  el:"#app",
   router,
   render: h => h(App)
 }).$mount('#app')
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,29 +1,39 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Layout from '@/pages/layout'
+// import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+export default new VueRouter({
+  routes: [
+    {
+      path: '/',
+      name: 'Layout',
+      component: Layout
+    }
+  ]
 })
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'Home',
+//     component: Home
+//   },
+//   {
+//     path: '/about',
+//     name: 'About',
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+//   }
+// ]
 
-export default router
+// const router = new VueRouter({
+//   mode: 'history',
+//   base: process.env.BASE_URL,
+//   routes
+// })
+
+// export default router
